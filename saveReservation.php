d797b4811223e194c3418ffb207bb6f7e22ea83a
Mariann Paska-Fáth <mannika3@gmail.com>
2024-04-11 21:20:43 +0200

    status-0 a foglalás után-adminisztrátori jóváhagyásra vár

--- saveReservation.php
+++ saveReservation.php
@@ -7,7 +7,7 @@ if(isset($_GET['userId']) && isset($_GET['selectedDateId']) && isset($_GET['sele
 
     $sqlReservation = "INSERT INTO reservation (user_id, place_id, date_id, status) VALUES (?, ?, ?, ?)";
     $queryReservation = $dbconn->prepare($sqlReservation);
-    $queryReservation->execute([$userId, $selectedHelyId, $selectedDateId, 1]);
+    $queryReservation->execute([$userId, $selectedHelyId, $selectedDateId, 0]);
     header("Location: index.php?foglalas=1");
 } else {
     header("Location: index.php?foglalas=0");
