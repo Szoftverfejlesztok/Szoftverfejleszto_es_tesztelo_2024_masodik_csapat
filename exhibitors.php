267dd2190fdecbb2ec0f6f5315db8e4498494f5d
Mariann84 <mannika3@gmail.com>
2024-04-13 09:06:54 +0200

    sql javítás

--- exhibitors.php
+++ exhibitors.php
@@ -40,7 +40,7 @@ session_start();
                         
                         
                           // Kiállítók lekérdezése
-                          $sqlKiallitok = "SELECT photo, name_company, product_description, online_availability FROM userdata;";
+                          $sqlKiallitok = "SELECT name_company, product_description, online_availability FROM userdata;";
                           $queryKiallitok = $dbconn->prepare($sqlKiallitok);
                           $queryKiallitok->execute();
                         
