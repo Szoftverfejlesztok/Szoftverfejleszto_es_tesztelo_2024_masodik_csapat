20baaa5c4033c61ea57cd26be9e47a59b1430b19
Mariann84 <mannika3@gmail.com>
2024-04-17 20:34:57 +0200

    adatbázis módosítás(date_vasar -> date_market), helyek átnevezése a rajznak megfelelően(place_number). Piac rajz átnevezése

--- market.sql
+++ market.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Már 26. 21:17
+-- Létrehozás ideje: 2024. Ápr 17. 19:53
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -18,32 +18,21 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `vasar`
+-- Adatbázis: `market`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `date_vasar`
+-- Tábla szerkezet ehhez a táblához `date_market`
 --
 
-CREATE TABLE `date_vasar` (
+CREATE TABLE `date_market` (
   `date_id` int(11) NOT NULL,
   `date` date NOT NULL,
-  `is_next` tinyint(1) DEFAULT 0 NOT NULL
+  `is_next` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `kinalat`
---
-
-CREATE TABLE `product_range` (
-  `product_range_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -53,10 +42,32 @@ CREATE TABLE `product_range` (
 
 CREATE TABLE `place` (
   `place_id` int(11) NOT NULL,
-  `place_number` tinyint(4) NOT NULL,
+  `place_number` varchar(10) NOT NULL,
   `place_price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `product_category` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `product_range`
+--
+
+CREATE TABLE `product_range` (
+  `product_range_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -71,19 +82,6 @@ CREATE TABLE `reservation` (
   `date_id` int(11) NOT NULL,
   `status` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `termek`
---
-
-CREATE TABLE `product` (
-  `product_id` int(11) NOT NULL,
-  `product_category` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
-
 
 -- --------------------------------------------------------
 
@@ -101,19 +99,33 @@ CREATE TABLE `userdata` (
   `email` varchar(50) NOT NULL,
   `online_availability` varchar(500) DEFAULT NULL,
   `product_description` varchar(300) NOT NULL,
-  `moderator` tinyint(1) DEFAULT 0 NOT NULL,
-  `status` tinyint(1) DEFAULT 0 NOT NULL
+  `moderator` tinyint(1) NOT NULL DEFAULT 0,
+  `status` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
+--
 -- Indexek a kiírt táblákhoz
 --
 
 --
--- A tábla indexei `date_vasar`
+-- A tábla indexei `date_market`
 --
-ALTER TABLE `date_vasar`
-  ADD PRIMARY KEY (`date_id`);
+ALTER TABLE `date_market`
+  ADD PRIMARY KEY (`date_id`),
+  ADD UNIQUE KEY `date` (`date`);
+
+--
+-- A tábla indexei `place`
+--
+ALTER TABLE `place`
+  ADD PRIMARY KEY (`place_id`);
+
+--
+-- A tábla indexei `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`),
+  ADD UNIQUE KEY `product_category` (`product_category`);
 
 --
 -- A tábla indexei `product_range`
@@ -124,12 +136,6 @@ ALTER TABLE `product_range`
   ADD KEY `product_id` (`product_id`);
 
 --
--- A tábla indexei `place`
---
-ALTER TABLE `place`
-  ADD PRIMARY KEY (`place_id`);
-
---
 -- A tábla indexei `reservation`
 --
 ALTER TABLE `reservation`
@@ -137,13 +143,6 @@ ALTER TABLE `reservation`
   ADD KEY `user_id` (`user_id`),
   ADD KEY `place_id` (`place_id`),
   ADD KEY `date_id` (`date_id`);
-
---
--- A tábla indexei `termek`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`product_id`),
-  ADD UNIQUE KEY `product_category` (`product_category`);
 
 --
 -- A tábla indexei `userdata`
@@ -157,47 +156,47 @@ ALTER TABLE `userdata`
 --
 
 --
--- AUTO_INCREMENT a táblához `date_vasar`
+-- AUTO_INCREMENT a táblához `date_market`
 --
-ALTER TABLE `date_vasar`
-  MODIFY `date_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-
---
--- AUTO_INCREMENT a táblához `kinalat`
---
-ALTER TABLE `product_range`
-  MODIFY `product_range_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+ALTER TABLE `date_market`
+  MODIFY `date_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT a táblához `place`
 --
 ALTER TABLE `place`
-  MODIFY `place_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `place_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `product_range`
+--
+ALTER TABLE `product_range`
+  MODIFY `product_range_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT a táblához `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-
---
--- AUTO_INCREMENT a táblához `termek`
---
-ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT a táblához `userdata`
 --
 ALTER TABLE `userdata`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Megkötések a kiírt táblákhoz
 --
 
 --
--- Megkötések a táblához `kinalat`
+-- Megkötések a táblához `product_range`
 --
 ALTER TABLE `product_range`
   ADD CONSTRAINT `product_range_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `userdata` (`user_id`),
@@ -209,8 +208,8 @@ ALTER TABLE `product_range`
 ALTER TABLE `reservation`
   ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `userdata` (`user_id`),
   ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`place_id`) REFERENCES `place` (`place_id`),
-  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`date_id`) REFERENCES `date_vasar` (`date_id`);
-
+  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`date_id`) REFERENCES `date_market` (`date_id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
