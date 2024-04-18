2f6523361e129e1777f13d9a741f7f44ef43af75
Kiss-Torma Krisztina <juhaszkriszti30@gmail.com>
2024-04-18 21:50:59 +0200

    profile módosítás

--- profile.php
+++ profile.php
@@ -4,6 +4,7 @@ $msg = "";
 
 require_once("dbconnect.php");
 session_start(); 
+class ProfileException extends Exception{}
 if (!isset($_SESSION["user"])){
     header("location:index.php");    
     exit;
@@ -24,25 +25,14 @@ function generateTable($dbconn){
                         $form .= '<form action="' . $_SERVER["PHP_SELF"] . '" method="POST">';
                         $form .= '<fieldset>';
 
-                        $form .= '<label>Felhasználónév: </label>';
-                        $form .= '<input type="text" name="user_name" id="user_name" readonly value="';
-                        $form .= $row["user_name"]; 
-                        $form .= '"><br>';
-
-                        $form .= '<label>E-mail cím: </label>';
-                        $form .= '<input type="text" name="email" id="email" value="';
-                        $form .= $row["email"]; 
-                        $form .= '"><br>';
-
-                        $form .= '<label>Jelszó: </label>';
-                        $form .= '<input type="password" name="password" id="password"><br>';
-
-                        $form .= '<label>Jelszó újra: </label>';
-                        $form .= '<input type="password" name="password_new" id="password_new"><br>';
-
-                        $form .= '<label>Cég név: </label>';
+                        $form .= '<label>Árus/Cég neve: *</label>';
                         $form .= '<input type="text" name="name_company" id="name_company" value="';
                         $form .= $row["name_company"]; 
+                        $form .= '"><br>';
+
+                        $form .= '<label>E-mail cím: *</label>';
+                        $form .= '<input type="email" name="email" id="email" value="';
+                        $form .= $row["email"]; 
                         $form .= '"><br>';
                         
                         $form .= '<label>Kapcsolattartó: </label>';
@@ -50,17 +40,17 @@ function generateTable($dbconn){
                         $form .= $row["contact"]; 
                         $form .= '"><br>';
                         
-                        $form .= '<label>Telefonszám: </label>';
-                        $form .= '<input type="tel" name="telephone" id="contact" value="';
+                        $form .= '<label>Telefonszám: * <br>(pl.: 06301234567)</label>';
+                        $form .= '<input type="tel" name="telephone" id="contact"  pattern="^06[0-9]{9}$" value="';
                         $form .= $row["telephone"]; 
                         $form .= '"><br>';
 
-                        $form .= '<label>Online elérhetőség: </label>';
-                        $form .= '<input type="text" name="online_availability" id="online_availability" value="';
+                        $form .= '<label>Online elérhetőség: <br>(pl.: https://www.pelda.com)</label>';
+                        $form .= '<input type="url" name="online_availability" id="online_availability" value="';
                         $form .= $row["online_availability"]; 
                         $form .= '"><br>';
 
-                        $form .= '<input type="submit" value="Módosít" name="submitModosit">';
+                        $form .= '<input type="submit" value="Módosít" name="submitModosit"><br><br>';
                         $form .= '</form>';
                         return $form;
                     }
@@ -69,6 +59,44 @@ function generateTable($dbconn){
             echo "Lekérdezési hiba: ".$e->getMessage();
             $error = "Lekérdezési hiba: ".$e->getMessage();
         }
+}
+function generateTable2($dbconn){
+    try {
+        if (!empty($dbconn)){
+            $sql = "SELECT user_name, email, name_company, contact, telephone, online_availability FROM userdata WHERE user_id=:user_id";  // a futtatandó sql utasítás
+            $query = $dbconn->prepare($sql);  // előkészített lekérdezés létrehozása
+            $query->bindValue("user_id", $_SESSION["user"]["user_id"], PDO::PARAM_STR);
+            $query->execute();  // lekérdezés futtatása
+            $form = "";
+                if ($query->rowCount()>0){  // a visszaadott sorok száma
+                    $row = $query->fetch(PDO::FETCH_ASSOC); 
+
+                    $form .= '<form action="' . $_SERVER["PHP_SELF"] . '" method="POST">';
+                    $form .= '<fieldset>';
+
+                    $form .= '<label>Felhasználónév: *</label>';
+                    $form .= '<input type="text" name="user_name" id="user_name" readonly value="';
+                    $form .= $row["user_name"]; 
+                    $form .= '"><br>';
+
+                    $form .= '<label>Jelenlegi jelszó: *</label>';
+                    $form .= '<input type="password" name="password" id="password"><br>';
+
+                    $form .= '<label>Új jelszó: *</label>';
+                    $form .= '<input type="password" name="password_new1" id="password_new1"><br>';
+
+                    $form .= '<label>Jelszó megerősítése: *</label>';
+                    $form .= '<input type="password" name="password_new2" id="password_new2"><br>';
+
+                    $form .= '<input type="submit" value="Módosít" name="submitModosit2">';
+                    $form .= '</form>';
+                    return $form;
+                }
+            }
+    } catch (PDOException $e){
+        echo "Lekérdezési hiba: ".$e->getMessage();
+        $error = "Lekérdezési hiba: ".$e->getMessage();
+    }
 }
 function updateUserProfile ($email, $name_company, $contact, $telephone, $online_availability, $dbconn){
     try {
@@ -93,6 +121,30 @@ function updateUserProfile ($email, $name_company, $contact, $telephone, $online
         $error = "Lekérdezési hiba: ".$e->getMessage();
     }
 }
+
+function updatePassword ($password, $password_new1, $password_new2, $dbconn){
+    if (!empty($dbconn))
+    {
+       if ($password_new1 != $password_new2) 
+       {
+        throw new ProfileException("Az új jelszó nem egyezik meg a jelszó megerősítésével!");
+       }
+       $sql = "SELECT password, user_id FROM  userdata WHERE user_name=:felhasznalonev";
+       $query = $dbconn->prepare($sql);
+       $query->bindValue("felhasznalonev", $_SESSION["user"]["user_name"], PDO::PARAM_STR);
+       $query->execute();
+       $user = $query->fetch(PDO::FETCH_ASSOC); //kiolvassuk az adatokat
+        if (!password_verify($password,$user["password"])){
+            throw new ProfileException("Hibás jelszó");
+        } 
+        $sql = "UPDATE userdata SET password=:password WHERE user_id=:user_id";
+        $query = $dbconn->prepare($sql);
+        $query->bindValue("password", password_hash($password, null), PDO::PARAM_STR);
+        $query->bindValue("user_id", $_SESSION["user"]["user_id"], PDO::PARAM_STR);
+        $query->execute();
+    }
+}
+
 if (isset($_POST["submitModosit"]) && !empty($dbconn)){   
     $email= trim($_POST["email"]); 
     $name_company= trim($_POST["name_company"]);
@@ -100,6 +152,21 @@ if (isset($_POST["submitModosit"]) && !empty($dbconn)){
     $telephone= trim($_POST["telephone"]);   
     $online_availability= trim($_POST["online_availability"]);
     updateUserProfile($email, $name_company, $contact, $telephone, $online_availability, $dbconn);
+}
+if (isset($_POST["submitModosit2"]) && !empty($dbconn)){   
+    $password= trim($_POST["password"]); 
+    $password_new1= trim($_POST["password_new1"]); 
+    $password_new2= trim($_POST["password_new2"]); 
+    try {
+        updatePassword($password, $password_new1, $password_new2, $dbconn);  
+        $msg = "Sikeres jelszó módosítás";  
+    } catch(ProfileException $e){
+        $error = "Hiba lépett fel a jelszómódosítás közben.".$e->getMessage();
+    } catch (PDOException $e){
+        $error = "Adatbázis hiba: ".$e->getMessage(); 
+    }  catch (Exception $e) {
+        $error = "Hiba lépett fel:".$e->getMessage();
+    }
 }
 
 ?>
@@ -132,8 +199,18 @@ if (isset($_POST["submitModosit"]) && !empty($dbconn)){
                     <h2>Profil adatok</h2>
                     <div class="profile-page">
                         <div class="form profile-form">
+                            <h3>Személyes adatok</h3>
                             <?php
                             $form = generateTable($dbconn);
+                            if ($form != null){
+                                echo $form;
+                            } else {
+                                echo "Nincs elérhető felhasználói profil.";
+                            }
+                            ?>
+                             <h3>Felhasználói adatok</h3>
+                            <?php
+                            $form = generateTable2($dbconn);
                             if ($form != null){
                                 echo $form;
                             } else {
